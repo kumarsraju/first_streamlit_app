e1a524f8e205a4819c7892bfd34a8347863a4a01
SKumars <kumarsbk77@gmail.com>
2023-05-04 11:26:49 -0700

    Update streamlit_app.py
    added cake menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
 streamlit.title('My family Bakery')
+streamlit.header('Cakes Menu')
+streamlit.text('Chocolate')
+streamlit.text('tres Les')
+streamlit.text('Vanilla')
+streamlit.text('Pineapple')
+
