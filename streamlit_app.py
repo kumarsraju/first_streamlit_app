1b2f2277df51ad8a801137b258b0d91c81279c78
SKumars <kumarsbk77@gmail.com>
2023-05-04 11:38:41 -0700

    Update streamlit_app.py
    added select options code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My family Bakery')
 streamlit.header('Cakes Menu')
@@ -16,6 +15,15 @@ streamlit.text('🥣 Soup')
 streamlit.text('🥗 Salad')
 streamlit.text('🐔 Omlettes')
 streamlit.text('🥑🍞 Avacado toast')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 
 streamlit.dataframe(my_fruit_list)
 
