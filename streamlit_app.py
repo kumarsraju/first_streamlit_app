ae23702f81c2908a53ee3dcae534325cfce4c195
SKumars <kumarsbk77@gmail.com>
2023-05-04 11:35:42 -0700

    Update streamlit_app.py
    added pandas, and fruit list data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My family Bakery')
 streamlit.header('Cakes Menu')
 streamlit.text('Chocolate')
@@ -12,4 +16,6 @@ streamlit.text('🥣 Soup')
 streamlit.text('🥗 Salad')
 streamlit.text('🐔 Omlettes')
 streamlit.text('🥑🍞 Avacado toast')
+
+streamlit.dataframe(my_fruit_list)
 
